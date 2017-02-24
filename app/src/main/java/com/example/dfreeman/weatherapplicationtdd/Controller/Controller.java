7066712d63fb34dacf922e9dbc1639000070d975
dfreemanRIIS <dfreeman@riis.com>
2017-02-24 09:35:21 -0500

    Set up blank MVC template

---
+++ app/src/main/java/com/example/dfreeman/weatherapplicationtdd/Controller/Controller.java
@@ -0,0 +1,4 @@
+package com.example.dfreeman.weatherapplicationtdd.Controller;
+
+public class Controller {
+}
---
+++ app/src/main/java/com/example/dfreeman/weatherapplicationtdd/View/WeatherActivity.java
@@ -0,0 +1,15 @@
+package com.example.dfreeman.weatherapplicationtdd.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.dfreeman.weatherapplicationtdd.R;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}
